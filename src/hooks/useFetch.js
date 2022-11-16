f4a0bf4054afb3261ba2af19fc06136e8176f8a5
Ivan <ivanvqz103@gmail.com>
2022-11-15 22:51:40 -0600

    s

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,14 +3,12 @@ import { useEffect, useState } from "react"
 import { API_URL } from "../constants/env"
 
 const useFetch = (endpoint, headers={}) => {
-    // endpoint: para que el hook pueda recibir el endpoint de la API
-    // headers: para peticiones de administración
 
     const [data, setData] = useState()
     const [error, setError] = useState()
     const [loading, setLoading] = useState(true)
 
-    // useEffect para que corra cuando la petición se haga
+    // useEffect for a petition
     useEffect(() => {
         axios
             .get(`${API_URL}/${endpoint}`) // endpoint es el endpoint de la API
@@ -21,7 +19,7 @@ const useFetch = (endpoint, headers={}) => {
                 setError(err)
             })
             .finally(() => {
-                setLoading(false) // para que no se quede cargando
+                setLoading(false) // for loading state
             })
     },[])
     return { data, error, loading }
